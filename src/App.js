994d3e9e90396317ef5afb3b970f0c003c90352b
aman-jain <amanjain@Amans-MacBook-Pro.local>
2018-05-23 21:02:18 -0700

    Made changes to update README.md file

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ImageHolder from './components/ImageHolder';
+import Toggle from './components/Toggle';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <p className="App-intro">
+          <h4>Component Pattern</h4>
           <ImageHolder>
             <ImageHolder.Image imageSrc='https://images.unsplash.com/photo-1523973814736-0ac95210c2fe' size='small-image'/>
             <ImageHolder.Description/>
-          </ImageHolder>  
+          </ImageHolder> 
+          <Toggle />
         </p>
       </div>
     );
@@ -19,3 +22,5 @@ class App extends Component {
 }
 
 export default App;
+
+
