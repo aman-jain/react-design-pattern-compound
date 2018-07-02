739c8e98c9c1c791b6f30226481ffedddda85b65
aman-jain <amanjain@Amans-MacBook-Pro.local>
2018-07-01 19:30:50 -0700

    Made changes to remove the toggle reference

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ImageHolder from './components/ImageHolder';
-import Toggle from './components/Toggle';
+
 
 class App extends Component {
   render() {
@@ -14,7 +14,7 @@ class App extends Component {
             <ImageHolder.Image imageSrc='https://images.unsplash.com/photo-1523973814736-0ac95210c2fe' size='small-image'/>
             <ImageHolder.Description/>
           </ImageHolder> 
-          <Toggle />
+        
         </p>
       </div>
     );
